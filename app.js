9e00e2cd6862a022d185d30feb58f6a5a4a9daf7
laxman-pajjuri <52751972+laxman-pajjuri@users.noreply.github.com>
2019-07-24 18:46:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/hello', function(req, res){
-   res.send("Hello World!");
+   res.send("Hello All");
 });
 
 app.post('/hello', function(req, res){
